afbbf95a16f4adfb9fe00372f7c3587911b2ff4a
Gerardo LSJ <gerardolsj@gmail.com>
2018-09-08 10:37:06 -0500

    serielizer

--- mxhacks/serializers.py
+++ mxhacks/serializers.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import(Tags, Procedure, Law,City,Country)
 from rest_framework import serializers
 
+from rest_framework.fields import ListField
+
+class StringArrayField(ListField):
+    """
+    String representation of an array field.
+    """
+    def to_representation(self, obj):
+    	obj = super().to_representation( obj)
+    	return ",".join([str(element) for element in obj])
+
+    def to_internal_value(self, data):
+    	data = data.split(",")  # convert string to list
+    	return super().to_internal_value(self, data)
+
 class TagsSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Tags
@@ -13,6 +27,7 @@ class ProcedureSerializer(serializers.ModelSerializer):
 		fields = ('__all__')
 
 class LawSerializer(serializers.ModelSerializer):
+	bullets = StringArrayField()
 	class Meta:
 		model = Law
 		fields = ('__all__')
